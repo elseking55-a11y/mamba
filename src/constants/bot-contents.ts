5b5cbd300bc25d32a94840a163192aa085883308
elseking55-a11y <elseking55@gmail.com>
2026-09-22 02:48:27 +0300

    remove Tutorials tab from Mamba navigation

--- src/constants/bot-contents.ts
+++ src/constants/bot-contents.ts
@@ -15,11 +15,10 @@ export const DBOT_TABS: TDashboardTabIndex = Object.freeze({
     DASHBOARD: 0,
     BOT_BUILDER: 1,
     CHART: 2,
-    TUTORIAL: 3,
 });
 
 export const MAX_STRATEGIES = 10;
 
-export const TAB_IDS = ['id-dbot-dashboard', 'id-bot-builder', 'id-charts', 'id-tutorials'];
+export const TAB_IDS = ['id-dbot-dashboard', 'id-bot-builder', 'id-charts'];
 
 export const DEBOUNCE_INTERVAL_TIME = 500;
